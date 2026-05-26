ef396b26c3412126ca31d427623b7b1e61fcb90f
Emily Bastidas <enily3@gmail.com>
2026-05-26 17:39:12 -0400

    actualización frot

--- todo_app/frontend/src/App.jsx
+++ todo_app/frontend/src/App.jsx
@@ -1,14 +1,20 @@
-/*import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "./assets/vite.svg";
-import heroImg from "./assets/hero.png";
-import "./App.css";*/
+import { useState } from "react";
+/*import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'*/
+import "./App.css";
 
 function App() {
+  const [tareas, setTareas] = useState([]);
+
   return (
-    <div className="container mt-5">
-      <h1>Hola mundo</h1>
-    </div>
+    <>
+      <h1>TodoList</h1>
+      <input type="text" placeholder="Add a new task" />
+      <button>Add Task</button>
+      <ol>Tarea 1</ol>
+      <ol>Tarea 2</ol>
+    </>
   );
 }
 
