c52699e72990e6f9c5ab3f60ea173a18bc485766
Emily Bastidas <enily3@gmail.com>
2026-05-26 21:52:36 -0400

    actualización

--- todo_app/frontend/src/App.jsx
+++ todo_app/frontend/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
 import Footer from "./components/Footer";
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <main className="flex-grow-1">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
 
           <Route
             path="*"
