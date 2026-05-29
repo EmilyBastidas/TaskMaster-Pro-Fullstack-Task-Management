382090133a0cfe64ee414989fe4cff27bd4aaec6
Emily Bastidas <enily3@gmail.com>
2026-05-28 21:32:52 -0400

    actualización login

--- todo_app/backend/src/app.js
+++ todo_app/backend/src/app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
-// middleware
+// middlewares
+app.use(cors());
 app.use(express.json());
 
 // rutas
