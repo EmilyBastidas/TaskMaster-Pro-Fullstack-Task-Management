b819aee16e8a260b92977f86791bc3131b5cbd30
Emily Bastidas <enily3@gmail.com>
2026-06-10 22:37:50 -0400

    actualización

--- todo_app/frontend/src/pages/Dashboard.jsx
+++ todo_app/frontend/src/pages/Dashboard.jsx
@@ -1,1 +1,2 @@
 //donde se guarda el historial de tareas, el usuario puede ver las tareas que ha creado, editarlas o eliminarlas. También puede cerrar sesión desde esta página.
+<h1>Bienvenido a tu dashboard</h1>;
