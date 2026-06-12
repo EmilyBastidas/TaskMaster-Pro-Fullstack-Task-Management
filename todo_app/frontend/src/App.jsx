45ccf97f99442fee70e75ee456fc8ffb8cab6121
Emily Bastidas <enily3@gmail.com>
2026-06-11 21:00:47 -0400

    arreglo navbar

--- todo_app/frontend/src/App.jsx
+++ todo_app/frontend/src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import { AuthProvider } from "./AuthContext";
 
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
@@ -11,31 +10,30 @@ import Task from "./pages/Task";
 
 function App() {
   return (
-    <AuthProvider>
-      <div className="d-flex flex-column min-vh-100">
-        <Navbar />
+    <div className="d-flex flex-column min-vh-100">
+      <Navbar />
 
-        <main className="flex-grow-1">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<Login />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/tasks" element={<Task />} />
-            <Route
-              path="*"
-              element={
-                <div className="text-center py-5">
-                  <h1>404 - Página no encontrada</h1>
-                  <p>Lo sentimos, esta página no existe.</p>
-                </div>
-              }
-            />
-          </Routes>
-        </main>
+      <main className="flex-grow-1">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/tasks" element={<Task />} />
 
-        <Footer />
-      </div>
-    </AuthProvider>
+          <Routes
+            path="*"
+            element={
+              <div className="text-center py-5">
+                <h1>404 - Página no encontrada</h1>
+                <p>Lo sentimos, esta página no existe.</p>
+              </div>
+            }
+          />
+        </Routes>
+      </main>
+
+      <Footer />
+    </div>
   );
 }
 
--- todo_app/frontend/src/components/AuthContext.js
+++ todo_app/frontend/src/components/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState } from "react";
+/*import { createContext, useState } from "react";
 
 export const AuthContext = createContext();
 
@@ -20,4 +20,4 @@ export const AuthProvider = ({ children }) => {
       {children}
     </AuthContext.Provider>
   );
-};
+};*/
