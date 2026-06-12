b6289245b8868c55dc721978070f68f3b58995e9
Emily Bastidas <enily3@gmail.com>
2026-06-11 20:59:17 -0400

    arreglo navbar

--- todo_app/frontend/src/components/Navbar.jsx
+++ todo_app/frontend/src/components/Navbar.jsx
@@ -1,16 +1,14 @@
-import { Link, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { FaCheckDouble } from "react-icons/fa6";
 import { IoLogOutSharp } from "react-icons/io5";
-import { useContext } from "react";
-import { AuthContext } from "../context/AuthContext";
 
-const Navbar = () => {
+function Navbar() {
   const navigate = useNavigate();
-  const { isAuthenticated, logout } = useContext(AuthContext);
 
   const handleLogout = () => {
-    logout();
-    navigate("/login");
+    localStorage.removeItem("token");
+    navigate("/");
   };
 
   return (
@@ -34,6 +32,9 @@ const Navbar = () => {
           type="button"
           data-bs-toggle="collapse"
           data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
@@ -45,37 +46,28 @@ const Navbar = () => {
                 Dashboard
               </Link>
             </li>
-
-            {!isAuthenticated && (
-              <>
-                <li className="nav-item">
-                  <Link className="nav-link text-white" to="/login">
-                    Inicio de sesión
-                  </Link>
-                </li>
-
-                <li className="nav-item">
-                  <Link className="nav-link text-white" to="/signup">
-                    Registrarse
-                  </Link>
-                </li>
-              </>
-            )}
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/login">
+                Inicio de sesión
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link text-white" to="/signup">
+                Registrarse
+              </Link>
+            </li>
           </ul>
         </div>
       </div>
 
-      {isAuthenticated && (
-        <IoLogOutSharp
-          className="p-3"
-          size={27}
-          style={{ cursor: "pointer", color: "#b6bcb9ff" }}
-          onClick={handleLogout}
-          title="Cerrar sesión"
-        />
-      )}
+      <IoLogOutSharp
+        size={27}
+        style={{ cursor: "pointer", color: "#e9eeebff" }}
+        onClick={handleLogout}
+        title="Cerrar sesión"
+      />
     </nav>
   );
-};
+}
 
 export default Navbar;
